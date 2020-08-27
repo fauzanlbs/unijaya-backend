e5d58acc7658f6022543d11888b4641f602cc7c5
Fauzan <fauzan_lubis23@yahoo.com>
2020-08-28 02:59:24 +0800

    add excel lib and update CRUD

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -25,21 +25,29 @@ class UserController extends Controller
     public function store(UserRequest $request)
     {
         $validated = $request->validated();
+        $user = User::create($request->all());
+        return response()->json(['success' => $user], $this->successStatus);
     }
 
     public function show($id)
     {
-
+        $user = User::find($id);
+        return response()->json(['success' => $user], $this->successStatus);
     }
 
     public function update(UserRequest $request, $id)
     {
         $validated = $request->validated();
+        $user = User::find($id);
+        $user->update($request->all());
+        return response()->json(['success' => $user], $this->successStatus);
     }
 
     public function destroy($id)
     {
-
+        $user = User::find($id);
+        $user->delete();
+        return response()->json(['success' => 'data berjaya dipadam'], $this->successStatus);
     }
 
     // CRUD END
